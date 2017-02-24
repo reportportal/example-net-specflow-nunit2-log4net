28c7c35d458c7f122ffc712a0f5cce8eba38e347
Shagbalov <Marat.Shagbalov@infotecs.ru>
2017-02-24 12:28:22 +0500

    Add scenario to send images into rp

---
+++ src/ReportPortal.Addins.SpecFlowPlugin.Sample/ImageSenderStepDefinitions.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+using ReportPortal.Addins.SpecFlowPlugin.Sample.Properties;
+using ReportPortal.Client.Models;
+using ReportPortal.Client.Requests;
+using ReportPortal.Shared;
+using System;
+using System.Drawing;
+using TechTalk.SpecFlow;
+
+namespace ReportPortal.Addins.SpecFlowPlugin.Sample
+{
+    [Binding]
+    public class ImageSenderStepDefinitions
+    {
+        private bool happy;
+
+        [Given(@"I have pug happy image")]
+        public void GivenIHavePugHappyImage()
+        {
+            Assert.IsNotNull(Resources.lucky);
+        }
+
+        [When(@"pug was happy")]
+        public void WhenPugWasHappy()
+        {
+            happy = true;
+        }
+
+        [Then(@"I send image to RP")]
+        public void ThenISendImageToRP()
+        {
+            byte[] image = getImageFromResources(happy);
+            Bridge.Service.AddLogItem(new AddLogItemRequest
+            {
+                TestItemId = Bridge.Context.TestId,
+                Text = happy ? "Pug happy" : "Pug unhappy",
+                Time = DateTime.UtcNow,
+                Attach = new Attach("Image", "image/jpeg", image),
+                Level = LogLevel.Info
+            });
+        }
+
+        [Given(@"I have pug unhappy image")]
+        public void GivenIHavePugUnhappyImage()
+        {
+            Assert.IsNotNull(Resources.unlucky);
+        }
+
+        [When(@"pug was unhappy")]
+        public void WhenPugWasUnhappy()
+        {
+            happy = false;
+        }
+
+        private byte[] getImageFromResources(bool happy)
+        {
+            var image = happy ? Resources.lucky : Resources.unlucky;
+            ImageConverter converter = new ImageConverter();
+            return (byte[])converter.ConvertTo(image, typeof(byte[]));
+        }
+    }
+}
---
+++ src/ReportPortal.Addins.SpecFlowPlugin.Sample/SpecFlowFeature4.feature
@@ -0,0 +1,14 @@
+﻿@sendimage
+Feature: SpecFlow Feature 4
+
+@happy
+Scenario: Pug happy
+	Given I have pug happy image
+	When pug was happy
+	Then I send image to RP
+
+@unhappy
+Scenario: Pug unhappy
+	Given I have pug unhappy image
+	When pug was unhappy
+	Then I send image to RP
